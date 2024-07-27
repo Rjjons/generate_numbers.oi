4afd827a0b6d3b3930228cfe7a33eaf1f315ca69
Jonah R. E. <joeetubi@gmail.com>
2024-07-27 19:05:43 +0100

    made some changes on the html, js, and css files.

--- script.js
+++ script.js
@@ -1,99 +1,95 @@
-
-function generate(){
-  function getRandomSegment(val) {
-    return Math.floor(Math.random() * 10000).toString().padStart(val, '0'); 
-}
-
-let segment1 = getRandomSegment(4);
-let segment2 = getRandomSegment(4);
-let segment3 = getRandomSegment(4);
-let segment4 = getRandomSegment(3);
-
-return `${segment1}-${segment2}-${segment3}-${segment4}`;     
-}
-  let result = ''; 
-
-function generateRechargeCard(){
-    
-const network = document.getElementById("network").value;
-  const deno = document.getElementById("enterDeno").value;
+function generateRechargeCard() {
   const qty = document.getElementById("enterQuantity").value;
-  
+  const deno = document.getElementById("enterDeno").value;
+  const network = document.getElementById("network").value;
 
-  if (network === 'mtn'){
-    // alert(network);
-  mtn( network,   deno, qty);
+  if (network === "mtn") {
+    //alert(network);
+    mtn(network, deno, qty);
   }
-  if( network === 'glo' ){
-    // alert(network);
-  glo( network, deno, qty );
+  if (network === "glo") {
+    //alert(network);
+    glo(network, deno, qty);
   }
-  if(network === 'airtel'){
-    // alert(network);
-  airtel( network, deno, qty );
+  if (network === "airtel") {
+    //alert(network);
+    airtel(network, deno, qty);
   }
-  
-  if(network === 'nineMobile'){
-    // alert(network);
-  nineMobile( network, deno, qty );
+
+  if (network === "nineMobile") {
+   // alert(network);
+    nineMobile(network, deno, qty);
   }
-  
 }
 
-function mtn(network, deno, qty){
-for(let i = 0; i < qty; i++ ){
-result += `
+// function to generate the random numbers
+function generate() {
+  function getRandomSegment(val) {
+    return Math.floor(Math.random() * 10000).toString().padStart(val, "0"); 
+  }
+
+  let segment1 = getRandomSegment(4);
+  let segment2 = getRandomSegment(4);
+  let segment3 = getRandomSegment(4);
+  let segment4 = getRandomSegment(3);
+
+  return `${segment1}-${segment2}-${segment3}-${segment4}`;
+}
+let result = "";
+
+function mtn(network, deno, qty) {
+  for (let i = 0; i < qty; i++) {
+    result += `
   <div class="card-container">
 <p><b>Network:</b>MTN</p>
 <p><b>Amount:</b>${deno}</p>
-<p><b>PIN:</b>${generate()}</p>
+<p><b>PIN:</b>${ generate()}</p>
 </div>
-  `
+  `;
+  }
 
+  document.getElementById("output").innerHTML = result;
 }
 
-document.getElementById("output").innerHTML=result;
-}
-
-function glo(network,deno,qty){
-  for (let i = 0; i < qty; i++){
+function glo(network, deno, qty) {
+  for (let i = 0; i < qty; i++) {
     result += `
   <div class="card-container">
 <p><b>Network:</b>GLO</p>
 <p><b>Amount:</b>${deno}</p>
 <p><b>PIN:</b>${generate()}</p>
 </div>
-  `
-
+  `;
   }
 
-document.getElementById("output").innerHTML=result;
+  document.getElementById("output").innerHTML = result;
 }
 
-function airtel(network,deno,qty){
-  for(i = 0; i < qty; i++){
-
+function airtel(network, deno, qty) {
+  for (i = 0; i < qty; i++) {
     result += `
   <div class="card-container">
 <p><b>Network:</b>Airtel</p>
 <p><b>Amount:</b>${deno}</p>
 <p><b>PIN:</b>${generate()}</p>
 </div>
-  `
+  `;
   }
 
-document.getElementById("output").innerHTML=result;
+  document.getElementById("output").innerHTML = result;
 }
 
-function nineMobile(network,deno,qty){
-for (i = 0; i < qty; i++){
-  result += `
+function nineMobile(network, deno, qty) {
+  for (i = 0; i < qty; i++) {
+    result += `
   <div class="card-container">
 <p><b>Network:</b>9mobile</p>
 <p><b>Amount:</b>${deno}</p>
-<p><b>PIN:</b>${generate()}</p>
+<p><b>PIN:&p </b>${generate()}</p>
 </div>
-  `
+  `;
+  }
+  document.getElementById("output").innerHTML = result;
 }
-document.getElementById("output").innerHTML=result;
-}
+
+
